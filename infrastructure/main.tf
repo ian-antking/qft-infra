5b2003dc0ff8b0b9fcc9ca1fc972abd0d5d68923
Ian King <ian.antking@gmail.com>
2022-11-10 11:44:10 +0000

    add minimum terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.55"
+    }
+  }
+  backend "s3" {
+    bucket = "qft-tf-state"
+    key    = "qft-ft-state"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+  default_tags {
+    tags = {
+      app = "mastodon"
+    }
+  }
+}
