4437e2c05cae3aa926cac98c88068fabce8667cd
Ian King <ian.antking@gmail.com>
2022-11-11 11:04:23 +0000

    send messages to sns topic and subscribe topic to email endpoint

--- services/sendMessage/index.js
+++ services/sendMessage/index.js
@@ -1,10 +1,30 @@
-const fetch = require('node-fetch')
+const { SNS } = require("aws-sdk")
 
 exports.handler = async event => {
     const { body } = event
-    const response = await fetch(process.env.SLACK_MESSAGE_ENDPOINT, {
-        method: 'POST',
-        body
-    })
-    return response
+    const { SNS_TOPIC } = process.env
+    
+
+    const sns = new SNS()
+
+    try {
+        const { text } = JSON.parse(body)
+
+        const params = {
+            Message: text,
+            TopicArn: SNS_TOPIC,
+            Subject: 'New contact from QFT website'
+        }
+    
+        await sns.publish(params).promise()
+    } catch (err) {
+        console.log(err)
+        return {
+            statusCode: 500,
+            body: JSON.stringify(err)
+        }
+    }
+    return {
+        statusCode: 200
+    }
 }
